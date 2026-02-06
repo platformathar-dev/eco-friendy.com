e501c35efe9a32b95496ecbd56c7e908ca5a96ff
platformathar-dev <platform.athar@gmail.com>
2026-02-06 22:39:39 +0300

    Create mailer.php

---
+++ public/mail/mailer.php
@@ -0,0 +1,53 @@
+<?php
+// استدعاء مكتبات PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// مسارات ملفات PHPMailer
+require __DIR__ . '/PHPMailer/Exception.php';
+require __DIR__ . '/PHPMailer/PHPMailer.php';
+require __DIR__ . '/PHPMailer/SMTP.php';
+
+/**
+ * دالة إرسال إيميل
+ * @param string $to      البريد المستلم
+ * @param string $subject عنوان الإيميل
+ * @param string $message محتوى الإيميل (HTML)
+ * @return bool
+ */
+function sendMail($to, $subject, $message)
+{
+    $mail = new PHPMailer(true);
+
+    try {
+        // إعداد SMTP
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.hostinger.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'info@eco-friendy.com';      // إيميلك
+        $mail->Password   = 'Abdullah@#$%27887';   // 🔴 كلمة السر (غيّرها)
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL
+        $mail->Port       = 465;
+        $mail->CharSet    = 'UTF-8';
+
+        // المرسل
+        $mail->setFrom('info@eco-friendy.com', 'Eco Friendy');
+
+        // المستلم
+        $mail->addAddress($to);
+
+        // محتوى الإيميل
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+
+        // إرسال
+        $mail->send();
+        return true;
+
+    } catch (Exception $e) {
+        // في حال الخطأ
+        // error_log($mail->ErrorInfo); // يمكنك تفعيله للتشخيص
+        return false;
+    }
+}
