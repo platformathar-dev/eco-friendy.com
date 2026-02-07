4539f1010e229aec18538a5c7647a4d476946756
platformathar-dev <platform.athar@gmail.com>
2026-02-07 04:34:54 +0300

    Update new_order_hook.php

--- public/hooks/new_order_hook.php
+++ public/hooks/new_order_hook.php
@@ -5,7 +5,7 @@
  */
 
 require_once __DIR__ . '/../EmailNotificationSystem.php';
-require_once __DIR__ . '/../db_config.php';
+require_once __DIR__ . '/../config.php';
 
 /**
  * استخدم هذه الدالة بعد نجاح إنشاء طلب جديد
