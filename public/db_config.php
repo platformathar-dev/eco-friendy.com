92f7fb6f00080578291d38b279539dfb9caf5fb4
platformathar-dev <platform.athar@gmail.com>
2026-02-07 04:41:06 +0300

    Update db_config.php

--- public/db_config.php
+++ public/db_config.php
@@ -8,9 +8,11 @@
 
 // معلومات الاتصال بقاعدة البيانات
 define('DB_HOST', 'localhost');
-define('DB_USER', 'your_username');
-define('DB_PASS', 'your_password');
-define('DB_NAME', 'your_database_name');
+define('DB_NAME', 'u407057164_ecofriendy');
+define('DB_USER', 'u407057164_ecofriendy');
+define('DB_PASS', 'Abazid@#$%27887');
+define('DB_CHARSET', 'utf8mb4');
+
 
 // إنشاء الاتصال
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
